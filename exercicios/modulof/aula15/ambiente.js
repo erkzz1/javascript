0c008c507b12f6f25dab25ac72dcad3b7667aaab
Erick Mathias <101367588+erkzz1@users.noreply.github.com>
2024-04-03 03:58:45 -0300

    aula15att

---
+++ exercicios/modulof/aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3]
+num[3] = 6
+num.push(7)
+console.log(`Quantos elementos eu possuo dentro do meu array: ${num.length} e quais são ${num}`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor em ordem crescente fica assim: ${num.sort()}`)
+
+
+let pos = num.indexOf(5)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
