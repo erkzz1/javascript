0598c8ead5094fbcbc675cd7927459fe3cbcdbee
Erick Mathias <101367588+erkzz1@users.noreply.github.com>
2024-04-03 04:06:36 -0300

    att

--- exercicios/modulof/aula15/ambiente.js
+++ exercicios/modulof/aula15/ambiente.js
@@ -6,9 +6,9 @@ console.log(`O primeiro valor do vetor é ${num[0]}`)
 console.log(`O vetor em ordem crescente fica assim: ${num.sort()}`)
 
 
-let pos = num.indexOf(5)
+let pos = num.indexOf(7)
 if (pos == -1) {
     console.log('O valor não foi encontrado!')
 } else {
-    console.log(`O valor está na posição ${pos}`)
+    console.log(`O valor ${num[pos]} está na posição ${pos}`)
 }
